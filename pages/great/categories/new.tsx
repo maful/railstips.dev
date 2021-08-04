3945491f9b426c06047ce9790ce7e268728ef9b1
maful <mafulprayoga@gmail.com>
2021-08-05 06:01:31 +0700

    Use Category form

--- pages/great/categories/new.tsx
+++ pages/great/categories/new.tsx
@@ -1,18 +1,17 @@
 import * as React from 'react'
 import { useQueryClient } from 'react-query'
 import { useRouter } from 'next/router'
+
 import { LeftArrowIcon } from 'components/icons'
-import Alert from 'components/ui/Alert'
 import { categoryKeys, useCreateCategory } from '@/hooks/useCategories'
+import { CategoryForm } from '@/features/Categories'
+import { Category } from '@/models/category'
 
 function CategoriesNew(): JSX.Element {
   const queryClient = useQueryClient()
   const router = useRouter()
-  const [name, setName] = React.useState('')
-  const [rank, setRank] = React.useState('')
-  const [active, setActive] = React.useState(true)
 
-  const { mutate, isError, error, isSuccess } = useCreateCategory({
+  const { mutate, error, isSuccess } = useCreateCategory({
     onSuccess: () => {
       queryClient.invalidateQueries({
         queryKey: categoryKeys.lists(),
@@ -21,10 +20,7 @@ function CategoriesNew(): JSX.Element {
     },
   })
 
-  async function handleSubmit(
-    e: React.FormEvent<HTMLFormElement>
-  ): Promise<void> {
-    e.preventDefault()
+  async function handleSubmit({ rank, name, active }: Category): Promise<void> {
     mutate({ rank: Number(rank), name, active })
   }
 
@@ -45,61 +41,7 @@ function CategoriesNew(): JSX.Element {
           </div>
         </button>
         <div className="mt-8 max-w-md">
-          <form className="px-4 py-4" onSubmit={handleSubmit}>
-            <div className="grid grid-cols-1 gap-6">
-              {isError && (
-                <Alert>
-                  <p className="mx-3">{error.message}</p>
-                </Alert>
-              )}
-
-              <label className="block">
-                <span className="text-gray-700">Name</span>
-                <input
-                  required
-                  type="text"
-                  value={name}
-                  onChange={(e) => setName(e.target.value)}
-                  className="mt-2 block form-input w-full rounded-md border-gray-300 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50"
-                />
-              </label>
-              <label className="block">
-                <span className="text-gray-700">Rank</span>
-                <input
-                  required
-                  type="number"
-                  value={rank}
-                  onChange={(e) => setRank(e.target.value)}
-                  className="mt-2 block form-input w-full rounded-md border-gray-300 shadow-sm focus:border-red-300 focus:ring focus:ring-red-200 focus:ring-opacity-50"
-                />
-              </label>
-              <div className="block">
-                <div className="mt-2">
-                  <div>
-                    <label className="inline-flex items-center">
-                      <input
-                        type="checkbox"
-                        checked={active}
-                        onChange={() => setActive((prevState) => !prevState)}
-                        className="form-checkbox rounded border-gray-300 text-red-600 shadow-sm focus:border-red-300 focus:ring focus:ring-offset-0 focus:ring-red-200 focus:ring-opacity-50"
-                      />
-                      <span className="ml-2">Enable the category</span>
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div className="block">
-                <div className="text-right">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Save
-                  </button>
-                </div>
-              </div>
-            </div>
-          </form>
+          <CategoryForm error={error?.message} handleSubmit={handleSubmit} />
         </div>
       </div>
     </div>
